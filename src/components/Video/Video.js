1e37329e8dfb1ccfb38118437c608de1aedcd6b3
nkand84 <nityabhagi@gmail.com>
2018-05-14 22:14:04 -0700

    call back fix

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,62 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 import YouTube from 'react-youtube';
 
 
-class Video extends React.Component {
-    // state = {
-    //     pause: 0
-    // }
+let youTubeTimer;
 
-    render() {
-        const opts = {
-            height: '390',
-            width: '640',
-            playerVars: { // https://developers.google.com/youtube/player_parameters
-                autoplay: 1
-            }
-        };
+class Video extends Component {
 
-        return (
-            <YouTube
-                videoId="2g811Eo7K8U"
-                opts={opts}
-                onReady={this._onReady}
-                // onPlay={this._pause}
-            />
-        );
+  state = {
+    ct: 0
+  };
+
+  round = (number, precision) => {
+    let shift = (number, precision) => {
+      let numArray = ("" + number).split("e");
+      return +(numArray[0] + "e" + (numArray[1] ? (+numArray[1] + precision) : precision));
+    };
+    return shift(Math.round(shift(number, +precision)), -precision);
+  }
+
+  _onPlay = (event) => {
+    // access to player in all event handlers via event.target
+    const stoppingPoints = [5, 10, 15, 18];
+    // youTubeTimer = setInterval(() => {
+    //   console.log(this.round(event.target.getCurrentTime(), 1));
+    //   console.log(event.target.getCurrentTime())
+    //    if (this.round(event.target.getCurrentTime(), 1) === stoppingPoints[this.state.ct]){
+    //     // if (event.target.getCurrentTime() >= stoppingPoints[this.state.ct]){
+    //     this.setState({
+    //       ct: this.state.ct + 1
+    //     });
+
+    //     event.target.pauseVideo();
+    //   }
+    // }, 100);
+
+    youTubeTimer = setInterval(() => {
+          event.target.pauseVideo();
+          console.log(event.target.getCurrentTime())
+        }
+      , 5000); 
     }
 
-    test(event) {
-        console.log("in test")  
-    }
-   
-    _onReady(event) {
-        // access to player in all event handlers via event.target
-        console.log("ready func");
-        // event.target.playVideo();
-        // event.target.pauseVideo()
-        setInterval(function(){
-            console.log(parseInt(event.target.getCurrentTime()));
-            if(parseInt(event.target.getCurrentTime()) === 5) {
-                event.target.pauseVideo();
-            }
-        }, 100);
-        
-        // event.target.getCurrentTime();
-        // setTimeout(event.target.pauseVideo,5000);
-    }
+  _onPause = (event) => {
+    clearInterval(youTubeTimer);
+  }
 
-    _pause(event){
-        console.log("in pause")
-        
-        setTimeout(this.test,5000);
-        // sleep(500).then(function(){
-        //     console.log("in test")
-        // });
-    }
+  _onEnd = (event) => {
+    this.setState({
+      ct: 0
+    });
+    clearInterval(youTubeTimer);
+  }
 
+  
 
-
+  render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1
+      }
+    };
+ 
+    return (
+      <YouTube
+        videoId="2g811Eo7K8U"
+        opts={opts}
+        onReady={this._onReady}
+        onPlay={this._onPlay}
+        onPause={this._onPause}
+        onEnd={this._onEnd}
+      />
+    );
+  }
 }
 
 
